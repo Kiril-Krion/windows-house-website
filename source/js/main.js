7a132230244493f4680dddb53af867425d0c4672
Kiril <stasiuk2003@gmail.com>
2022-05-14 19:46:51 +0300

    tabs functionality was added

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,19 @@ const tabItems = document.querySelectorAll('.about__tabs-item');
 
 tabsBtn.forEach(btn => {
     btn.addEventListener('click', () => {
-        btn.classList.toggle('is-active');
+        let currentBtn = btn;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+
+        tabsBtn.forEach(item => {
+            item.classList.remove('is-active');
+        })
+
+        tabItems.forEach(item => {
+            item.classList.remove('active-tab');
+        })
+
+        currentBtn.classList.add('is-active');
+        currentTab.classList.add('active-tab');
     })
 })
